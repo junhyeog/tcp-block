5ccd5eb92dad7b87364de2c32bed24b7cdc8b6c6
junhyeog <junhyeog@korea.ac.kr>
2020-12-01 04:32:23 +0900

    Fix struct IpHdr

--- iphdr.h
+++ iphdr.h
@@ -12,7 +12,7 @@ struct IpHdr final {
   uint8_t hdr_len : 4;
   uint8_t version : 4;
   uint8_t tos;
-  uint16_t total_len;
+  uint16_t len;
   uint16_t id;
   uint8_t ip_frag_offset : 5;
   uint8_t ip_more_fragment : 1;
@@ -28,7 +28,7 @@ struct IpHdr final {
   uint8_t version() { return version; }
   uint8_t hdr_len() { return hdr_len; }
   uint8_t tos() { return tos; }
-  uint16_t len() { return ntohs(total_len); }
+  uint16_t len() { return ntohs(len); }
   uint16_t id() { return ntohs(id); }
   uint8_t ttl() { return ip_ttl; }
   uint8_t protocol() { return ip_protocol; }
