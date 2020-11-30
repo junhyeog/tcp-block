d38362d9e8ff402cd06931e7003498da4dde50b8
junhyeog <junhyeog@korea.ac.kr>
2020-12-01 04:34:05 +0900

    Fix struct IpHdr

--- iphdr.h
+++ iphdr.h
@@ -9,30 +9,30 @@
 
 #pragma pack(push, 1)
 struct IpHdr final {
-  uint8_t hdr_len : 4;
-  uint8_t version : 4;
-  uint8_t tos;
-  uint16_t len;
-  uint16_t id;
-  uint8_t ip_frag_offset : 5;
-  uint8_t ip_more_fragment : 1;
-  uint8_t ip_dont_fragment : 1;
-  uint8_t ip_reserved_zero : 1;
-  uint8_t ip_frag_offset1;
-  uint8_t ip_ttl;
-  uint8_t ip_protocol;
-  uint16_t ip_checksum;
+  uint8_t hdr_len_ : 4;
+  uint8_t version_ : 4;
+  uint8_t tos_;
+  uint16_t len_;
+  uint16_t id_;
+  uint8_t frag_offset : 5;
+  uint8_t more_fragment : 1;
+  uint8_t dont_fragment : 1;
+  uint8_t reserved_zero : 1;
+  uint8_t frag_offset1;
+  uint8_t ttl_;
+  uint8_t protocol_;
+  uint16_t checksum_;
   Ip sip_;
   Ip dip_;
 
-  uint8_t version() { return version; }
-  uint8_t hdr_len() { return hdr_len; }
-  uint8_t tos() { return tos; }
-  uint16_t len() { return ntohs(len); }
-  uint16_t id() { return ntohs(id); }
-  uint8_t ttl() { return ip_ttl; }
-  uint8_t protocol() { return ip_protocol; }
-  uint16_t checksum() { return ntohs(ip_checksum); }
+  uint8_t version() { return version_; }
+  uint8_t hdr_len() { return hdr_len_; }
+  uint8_t tos() { return tos_; }
+  uint16_t len() { return ntohs(len_); }
+  uint16_t id() { return ntohs(id_); }
+  uint8_t ttl() { return ttl_; }
+  uint8_t protocol() { return protocol_; }
+  uint16_t checksum() { return ntohs(checksum_); }
 
   Ip sip() { return ntohl(sip_); }
   Ip dip() { return ntohl(dip_); }
